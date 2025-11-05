73051afb4da5fa3474bb9258dd022b7ef58cd192
srgio088-dev <srgio088@gmail.com>
2025-11-05 13:50:22 -0500

    Update main.jsx
    log in

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,21 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles.css'
+import netlifyIdentity from 'netlify-identity-widget';
+import axios from 'axios';
+
+netlifyIdentity.init({
+  APIUrl: 'https://virtualteacher.netlify.app/.netlify/identity',
+});
+
+netlifyIdentity.on('login', async (user) => {
+  const token = await user.jwt();
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+});
+
+netlifyIdentity.on('logout', () => {
+  delete axios.defaults.headers.common.Authorization;
+});
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
