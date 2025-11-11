e2eafa5b9a0517c58c6e2fd693d862d382e18867
srgio088-dev <srgio088@gmail.com>
2025-11-11 10:46:30 -0500

    Update client.js
    export default apiFetch;

--- src/api/client.js
+++ src/api/client.js
@@ -1,5 +1,7 @@
 export const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
+export default apiFetch;
+
 export async function apiGet(path) {
   const res = await fetch(`${API_BASE}${path}`);
   if (!res.ok) throw new Error(`GET ${path} failed: ${res.status}`);
