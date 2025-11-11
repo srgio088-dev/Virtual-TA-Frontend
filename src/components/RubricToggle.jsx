a23a9e630ed4da4ad19d9d2cb11b41f53cf670ca
srgio088-dev <srgio088@gmail.com>
2025-11-11 10:20:15 -0500

    Create RubricToggle.jsx

---
+++ src/components/RubricToggle.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import { apiFetch } from '../api/client';
+
+export default function RubricToggle({ assignmentId }) {
+  const [open, setOpen] = useState(false);
+  const [rubric, setRubric] = useState('');
+
+  async function toggle() {
+    if (!open && !rubric) {
+      const res = await apiFetch(`/assignments/${assignmentId}/rubric`);
+      setRubric(res.rubric || 'No rubric provided.');
+    }
+    setOpen(!open);
+  }
+
+  return (
+    <div className="mt-3">
+      <button className="btn" onClick={toggle}>
+        {open ? 'Hide Rubric' : 'View Rubric'}
+      </button>
+
+      {open && (
+        <div className="mt-2 p-3 border rounded bg-white whitespace-pre-wrap">
+          {rubric}
+        </div>
+      )}
+    </div>
+  );
+}
