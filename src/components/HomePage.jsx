9f4493d92cd4b438aa7c47c35a54153b05252ae7
alexxadavis <davisak10@appstate.edu>
2025-12-01 18:43:05 -0500

    Update HomePage.jsx

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -9,7 +9,7 @@ export default function Home() {
     <div style={styles.outer}>
       {/* Left side image */}
       <img
-        src="/images/Linn_Cove.webp"
+        src="/images/boone_campus.webp"
         alt="Left decoration"
         style={styles.sideImageLeft}
       />
@@ -69,7 +69,7 @@ export default function Home() {
 
       {/* Right side image */}
       <img
-        src="/images/Linn_Cove.webp"
+        src="/images/boone_campus.webp"
         alt="Right decoration"
         style={styles.sideImageRight}
       />
@@ -181,3 +181,4 @@ const styles = {
     color: "#555",
   },
 };
+
