39e8670fe0c5e98e98845ee2d3a70f75f790576e
alexxadavis <davisak10@appstate.edu>
2025-12-01 12:53:33 -0500

    Update ReviewSubmission.jsx

--- src/components/ReviewSubmission.jsx
+++ src/components/ReviewSubmission.jsx
@@ -169,7 +169,7 @@ export default function ReviewSubmission() {
         className="btn">
         Back to Submissions
         </button>
-    </div>
-  </form>
+      </form>
+      </div>
   );
 }
